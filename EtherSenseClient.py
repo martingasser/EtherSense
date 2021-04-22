d22027c725a694dca8f8d3f6dcb63698f24a0429
martingasser <martin.gasser@gmail.com>
2021-04-22 09:37:23 +0200

    refactored client

--- EtherSenseClient.py
+++ EtherSenseClient.py
@@ -26,50 +26,15 @@ mc_ip_address = '224.0.0.1'
 port = 1024
 chunk_size = 4096
 
-async def receive_from_zmq(zmq_socket, plugins):
-
+async def receive_from_zmq(zmq_socket, plugins, queue):
     received_data = {}
-
-    def process_data():
-        if 'pose_array' in received_data:
-            pose_array = received_data['pose_array']
-            translation = pose_array[0:3]
-            rotation = pose_array[3:7]
-            velocity = pose_array[7:10]
-            acceleration = pose_array[10:13]
-            angular_velocity = pose_array[13:16]
-            angular_acceleration = pose_array[16:19]
-        
-            translation_text = f'Translation: {translation[0]: 0.2f}, {translation[1]: 0.2f}, {translation[2]: 0.2f}'
-            rotation_text = f'Rotation: {rotation[0]: 0.2f}, {rotation[1]: 0.2f}, {rotation[2]: 0.2f}, {rotation[3]: 0.2f}'
-
-            if 'color_array' in received_data:
-                color_array = received_data['color_array']
-                cv2.putText(color_array, translation_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (65536), 2, cv2.LINE_AA)
-                cv2.putText(color_array, rotation_text, (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (65536), 2, cv2.LINE_AA)
-            
-        if 'yolo_features' in received_data:
-            plugin_features = received_data['yolo_features']
-            color = (255, 0, 0)
-            
-            classes = []
-            if 'color_array' in received_data:
-                color_array = received_data['color_array']
-                for (classname, score, box) in zip(plugin_features['classes'], plugin_features['scores'], plugin_features['boxes']):
-                    label = "%s : %f" % (classname, score)
-                    cv2.rectangle(color_array, box, color, 2)
-                    cv2.putText(color_array, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-
-        if 'color_array' in received_data:
-            if args.gui:
-                cv2.imshow("window", received_data['color_array'])
-
     while True:
         try:
             topic, data = await zmq_socket.recv_multipart()
             if topic == b'TIME':
                 if 'timestamp' in received_data:
-                    process_data()
+                    #res = process_data()
+                    queue.put_nowait(received_data)
                 received_data['timestamp'] = struct.unpack('<d', data[0:8])[0]
             elif topic == b'RGB':
                 received_data['color_array'] = pickle.loads(data)
@@ -84,13 +49,12 @@ async def receive_from_zmq(zmq_socket, plugins):
                     deserialized_features = plugins[plugin_id].deserialize_features(data)
                     received_data[plugin_features_name] = deserialized_features
 
-            if args.gui:
-                key = cv2.waitKey(1)
-                if key == 27:
-                    break
         except asyncio.CancelledError:
+            print('cancelled')
             raise
-
+    
+    loop = asyncio.get_running_loop()
+    loop.stop()
 
 async def send_ping(transport, address):
     while True:
@@ -101,11 +65,58 @@ async def send_ping(transport, address):
         except asyncio.CancelledError:
             raise
 
+def process_display_data(received_data):
+    if 'pose_array' in received_data:
+        pose_array = received_data['pose_array']
+        translation = pose_array[0:3]
+        rotation = pose_array[3:7]
+        velocity = pose_array[7:10]
+        acceleration = pose_array[10:13]
+        angular_velocity = pose_array[13:16]
+        angular_acceleration = pose_array[16:19]
+    
+        translation_text = f'Translation: {translation[0]: 0.2f}, {translation[1]: 0.2f}, {translation[2]: 0.2f}'
+        rotation_text = f'Rotation: {rotation[0]: 0.2f}, {rotation[1]: 0.2f}, {rotation[2]: 0.2f}, {rotation[3]: 0.2f}'
+
+        if 'color_array' in received_data:
+            color_array = received_data['color_array']
+            cv2.putText(color_array, translation_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (65536), 2, cv2.LINE_AA)
+            cv2.putText(color_array, rotation_text, (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (65536), 2, cv2.LINE_AA)
+        
+    if 'yolo_features' in received_data:
+        plugin_features = received_data['yolo_features']
+        color = (255, 0, 0)
+        
+        classes = []
+        if 'color_array' in received_data:
+            color_array = received_data['color_array']
+            for (classname, score, box) in zip(plugin_features['classes'], plugin_features['scores'], plugin_features['boxes']):
+                label = "%s : %f" % (classname, score)
+                cv2.rectangle(color_array, box, color, 2)
+                cv2.putText(color_array, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    return color_array
+
+
+async def display_data(queue):
+    while True:
+        received_data = await queue.get()
+        color_array = process_display_data(received_data)
+        if args.gui:
+            cv2.imshow("window", color_array)
+            key = cv2.waitKey(1)
+            # if key == 27:
+            #     break
+        queue.task_done()
+
+
 class DiscoveryClientProtocol:
     def __init__(self, loop):
         self.loop = loop
         self.transport = None
         self.ctx = None
+        
+        self.display_task = None
 
     def connection_made(self, transport):
         self.transport = transport
@@ -134,9 +145,10 @@ class DiscoveryClientProtocol:
             if args.plugins:
                 plugins = import_plugins(args.plugins)
             
-            self.receive_task = asyncio.ensure_future(receive_from_zmq(zmq_socket, plugins))
-            #self.receive_task.add_done_callback(lambda arg: self.transport.close())
-        
+            queue = asyncio.Queue()
+            self.receive_task = asyncio.ensure_future(receive_from_zmq(zmq_socket, plugins, queue))
+            if args.gui:
+                self.display_task = asyncio.ensure_future(display_data(queue))
 
     def error_received(self, exc):
         print('Error received:', exc)
@@ -146,13 +158,9 @@ class DiscoveryClientProtocol:
             self.ping_task.cancel()
         if self.receive_task:
             self.receive_task.cancel()
+        if self.display_task:
+            self.display_task.cancel()
 
-
-
-# def signal_handler(sig, frame):    
-#     sys.exit(0)
-    
-#signal.signal(signal.SIGINT, signal_handler)
 
 def main():
     loop = asyncio.get_event_loop()
